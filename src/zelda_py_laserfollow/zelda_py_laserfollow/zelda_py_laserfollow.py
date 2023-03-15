2c0f9a3d5e2efe8a57f66c21d68aacc44a9b15cf
Jinay Jain <jinaybjain@gmail.com>
2023-03-15 09:32:41 -0400

    Subscribe to laser topic

--- src/zelda_py_laserfollow/zelda_py_laserfollow/zelda_py_laserfollow.py
+++ src/zelda_py_laserfollow/zelda_py_laserfollow/zelda_py_laserfollow.py
@@ -4,6 +4,7 @@ import rclpy
 from geometry_msgs.msg import Twist
 
 from irobot_create_msgs.msg import HazardDetectionVector, HazardDetection
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from rclpy import qos
 
@@ -26,66 +27,19 @@ class LaserFollow(Node):
             qos.qos_profile_sensor_data)
         self.hazard_subscription  # prevent unused variable warning
 
-        self.publisher = self.create_publisher(Twist, 'zelda/cmd_vel', 10)
-        self.move_timer = self.create_timer(
-            TIMER_INTERVAL,
-            self.move_timer_callback
-        )
-        self.backup_timer = None
-        self.spin_timer = None
+        self.laser_subscription = self.create_subscription(
+            LaserScan,
+            'zelda/scan',
+            self.laser_callback,
+            qos.qos_profile_sensor_data)
 
-        self.move_state = "forward"  # forward, spin, backward
-
-    def move_timer_callback(self):
-        twist = Twist()
-
-        self.get_logger().info(f"moving {self.move_state}")
-
-        if self.move_state == "forward":
-            twist.linear.x = 0.1  # m/s
-        elif self.move_state == "spin":
-            twist.angular.z = 1.0  # rad/s
-        else:  # backward
-            twist.linear.x = -0.01  # m/s
-
-        self.publisher.publish(twist)
-
-    def backup_timer_callback(self):
-        self.move_state = "spin"
-        self.backup_timer.destroy()
-
-        min_spin_time = radians(120)
-        max_spin_time = radians(240)
-
-        spin_time = uniform(min_spin_time, max_spin_time)
-
-        self.spin_timer = self.create_timer(
-            spin_time,
-            self.spin_timer_callback
-        )
-
-    def spin_timer_callback(self):
-        self.move_state = "forward"
-        self.spin_timer.destroy()
+        self.laser_subscription  # prevent unused variable warning
 
     def hazard_callback(self, haz: HazardDetectionVector):
-        if self.move_state != "forward":
-            return
+        print(f"Received hazard: {haz}")
 
-        # self.get_logger().info('hazard!: "%i"' % len(haz.detections))
-
-        for det in haz.detections:
-            if det.type == HazardDetection.BUMP:
-                self.get_logger().info(f"bumped")
-
-                # stop the robot
-                twist = Twist()
-                self.publisher.publish(twist)
-
-                # start backing up
-                self.move_state = "backward"
-                self.backup_timer = self.create_timer(
-                    BACKUP_TIME, self.backup_timer_callback)
+    def laser_callback(self, scan: LaserScan):
+        print(f"Received laser: {scan}")
 
 
 def main(args=None):
