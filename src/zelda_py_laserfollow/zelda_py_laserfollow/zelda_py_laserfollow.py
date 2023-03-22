9f2759081f84643f7056c8345bcaf21a4cf17b9d
mghansen <mghansen@udel.edu>
2023-03-22 09:58:39 -0400

    forward code initial

--- src/zelda_py_laserfollow/zelda_py_laserfollow/zelda_py_laserfollow.py
+++ src/zelda_py_laserfollow/zelda_py_laserfollow/zelda_py_laserfollow.py
@@ -9,7 +9,7 @@ from geometry_msgs.msg import Twist, Point
 from visualization_msgs.msg import Marker
 from rclpy import qos
 
-from math import cos, sin, pi
+from math import cos, sin, pi, degrees
 from random import uniform
 
 TIMER_INTERVAL = 0.5
@@ -40,6 +40,41 @@ class LaserFollow(Node):
             Marker,
             'zelda/marker',
             qos.qos_profile_sensor_data)
+        
+        self.move_publisher = self.create_publisher(Twist, 'zelda/cmd_vel', 10)
+        self.move_timer = self.create_timer(
+            TIMER_INTERVAL,
+            self.move_timer_callback
+        )
+    
+    def move_timer_callback(self):
+        twist = Twist()
+
+        self.get_logger().info(f"moving {self.move_state}")
+
+        if self.move_state == "forward":
+            twist.linear.x = 0.1  # m/s
+        elif self.move_state == "spin":
+            twist.angular.z = 1.0  # rad/s
+        else:  # backward
+            twist.linear.x = -0.01  # m/s
+
+        self.move_publisher.publish(twist)
+
+    def backup_timer_callback(self):
+        self.move_state = "spin"
+        self.backup_timer.destroy()
+
+        #min_spin_time = radians(120)
+        #max_spin_time = radians(240)
+
+        #spin_time = uniform(min_spin_time, max_spin_time)
+
+        self.spin_timer = self.create_timer(
+            spin_time,
+            self.spin_timer_callback
+        )
+    
 
     def hazard_callback(self, haz: HazardDetectionVector):
         #print(f"Received hazard: {haz}")
@@ -50,11 +85,15 @@ class LaserFollow(Node):
 
         for i, dist in enumerate(scan.ranges):
             
-            if dist > scan.range_min and dist < scan.range_max:
+            if dist > scan.range_min and dist < .5:
                 x = dist * cos(i * scan.angle_increment)
                 y = dist * sin(i * scan.angle_increment)
-                print(f"x: {x}")
-                print(f"y: {y}")
+                #print(f"x: {x}")
+                #print(f"y: {y}")
+
+            
+            if i/len(scan.ranges) < .25 and i/len(scan.ranges) > 0:
+                print(f"angle: {degrees(i*scan.angle_increment)}")
 
         marker = Marker()
 
